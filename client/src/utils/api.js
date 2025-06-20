8061d412528175b709968cdb9b5641a18bd0b825
Joshua Hester <jmhester20@vt.edu>
2025-06-20 19:37:12 -0400

    Test 06

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,11 +3,11 @@ import axios from 'axios';
 
 // Dynamically choose the base URL
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE || 'https://fitnesslog-backend.onrender.com/api'
+  baseURL: process.env.REACT_APP_API_BASE || 'https://fitnesslogapp-github-io.onrender.com/api'
 
 });
 
-// Attach the Authorization header on every request
+// Attach the Authorization header on every reques
 api.interceptors.request.use(
   config => {
     const token = localStorage.getItem('token');
