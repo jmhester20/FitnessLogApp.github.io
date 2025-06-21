4ae3417aed07f37e2d13efa10fcb70caf6c3a549
Joshua Hester <jmhester20@vt.edu>
2025-06-20 20:02:20 -0400

    Test 09

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Dynamically choose the base URL
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE || 'https://fitnesslogapp-github-io-1.onrender.com/api'
+  baseURL: 'https://fitnesslogapp-github-io-1.onrender.com/api'
 });
 
 // Attach the Authorization header on every reques
